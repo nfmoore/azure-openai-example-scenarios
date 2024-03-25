e28597b858028d83cb7bb8fe425d6f55a24c6c0f
Nicholas Moore <nfmoore@outlook.com>
2024-03-25 21:12:38 +1100

    Update streamlit app

--- src/app/main.py
+++ src/app/main.py
@@ -9,73 +9,63 @@ import sys
 import streamlit as st
 from dotenv import load_dotenv
 
-sys.path.append(os.path.join(os.getcwd(), "src"))
-
-from orchestration.utilities import OrchestrationClient
+from rag.utilities import RetrievalAugmentedGenerationClient
 
 
-def create_completion(client: OrchestrationClient, chat_history: dict):
+def get_answer(question: str) -> str:
     """
-    Create a completion using the client and messages.
+    Create a completion using the client and message history.
 
     Parameters:
-        client (OrchestrationClient): The orchestration client client.
-        messages (list): The list of messages.
+        client (RetrievalAugmentedGenerationClient): The rag client.
+        question (str): The latest user message.
 
     Returns:
-        chat_history: The chat history.
+        formatted_answer: The response to the the user's message.
     """
 
     # Generate a chat response and update chat history
-    chat_history = client.generate_chat_response(chat_history)
-    st.session_state.chat_history = chat_history
+    message_history = st.session_state.client.get_answer(
+        question=question, message_history=st.session_state.message_history
+    )
+
+    # Update message history for client
+    st.session_state.message_history = message_history
 
     # Get the assistant message from the chat history
-    assistant_message = chat_history["messages"][-1]["content"]
+    formatted_answer = replace_references(
+        answer=message_history[-1]["content"],
+        references=message_history[-2]["references"],
+    )
 
-    return assistant_message
+    return formatted_answer
 
 
-def replace_references(text: str, sources: list[dict]) -> str:
+def replace_references(answer: str, references: list[dict]) -> str:
     """
     Convert references in the format [*.md] to ``[*.md](path)``*.
 
     Parameters:
-        text (str): The text to modify.
+        answer (str): The text to modify.
+        references (list[dict]): The list of references.
 
     Returns:
-        modified_text: The modified text.
+        answer: The modified answer.
     """
-    try:
-        # Regex to match references in the format [*.md]
-        regex = r"\[([^\]]*.md)\]"
+    # Generate lookup table for references
+    reference_lookup = (
+        {item["title"]: item["path"] for item in references} if references else {}
+    )
 
-        if sources:
-            # Find matched references
-            references = re.findall(regex, text)
+    # Find and format all references in answer
+    references = re.findall(r"\[([^\]]*.md)\]", answer)
+    for reference in references:
+        answer = answer.replace(
+            f"[{reference}]",
+            f"[``{reference}``]({reference_lookup.get(reference)})",
+        )
 
-            # Replace matched references with modified references
-            for reference in references:
-                # Get the source path for the reference
-                source = list(filter(lambda x: x["title"] == reference, sources))[
-                    0
-                ].get("path")
-
-                if source:
-                    modified_text = text.replace(
-                        f"[{reference}]", f"[``{reference}``]({source})"
-                    )
-                else:
-                    modified_text = text.replace(f"[{reference}]", f"``{reference}``")
-        else:
-            # Replace matched references with modified references
-            modified_text = re.sub(regex, r"``\1``", text)
-
-        return modified_text
-
-    except Exception as e:
-        print(e)
-        return text
+    return answer
 
 
 def main():
@@ -91,34 +81,36 @@ def main():
     Returns:
         None
     """
-
-    st.title("Get your data talking!")
+    # Set the title of the app
+    st.title(os.getenv("AZURE_APP_TITLE"))
 
     # Initialize the orchestration client
-    st.session_state.client = OrchestrationClient(
+    st.session_state.client = RetrievalAugmentedGenerationClient(
         open_ai_endpoint=os.getenv("AZURE_OPENAI_API_BASE"),
         open_ai_chat_deployment=os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT"),
         open_ai_embedding_deployment=os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT"),
         search_endpoint=os.getenv("AZURE_AI_SEARCH_ENDPOINT"),
         search_index_name=os.getenv("AZURE_AI_SEARCH_INDEX_NAME"),
+        system_prompt_configuration_file="src/rag/configuration.yaml"
+        or os.getenv("AZURE_APP_SYSTEM_PROMPT_CONFIGURATION_FILE"),
     )
 
-    # Initialize chat history for app session (without source documents)
-    if "messages" not in st.session_state:
-        st.session_state.messages = [
+    # Initialize messages from app session
+    if "app_messages" not in st.session_state:
+        st.session_state.app_messages = [
             {"role": "assistant", "content": "How may I assist you today?"}
         ]
 
-    # Initialize chat history for orchastation client (with source documents)
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = {"messages": []}
+    # Initialize message history for client
+    if "message_history" not in st.session_state:
+        st.session_state.message_history = []
 
     # Initialize a flag for create_completion running status
     if "is_running" not in st.session_state:
         st.session_state.is_running = False
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
+    # Display messages from app session on app rerun
+    for message in st.session_state.app_messages:
         if message["role"] != "system":
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
@@ -128,10 +120,8 @@ def main():
         "Ask me a question", disabled=st.session_state.is_running
     ):
 
-        # Add user message to chat history
-        user_message = {"role": "user", "content": prompt}
-        st.session_state.messages.append(user_message)
-        st.session_state.chat_history["messages"].append(user_message)
+        # Add user message to app message history
+        st.session_state.app_messages.append({"role": "user", "content": prompt})
 
         # Display user message in chat message container
         with st.chat_message("user"):
@@ -141,27 +131,24 @@ def main():
         st.rerun()
 
     # Generate a new response if last message is not from assistant
-    if st.session_state.messages[-1]["role"] != "assistant":
+    if st.session_state.app_messages[-1]["role"] != "assistant":
+
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             with st.spinner("Generating response..."):
-                response = create_completion(
-                    st.session_state.client, st.session_state.chat_history
-                )
-
-                response = replace_references(
-                    response,
-                    st.session_state.chat_history["messages"][-2].get("references"),
+                response = get_answer(
+                    question=st.session_state.app_messages[-1]["content"]
                 )
 
             st.markdown(response)
             st.session_state.is_running = False
 
         # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.app_messages.append({"role": "assistant", "content": response})
         st.rerun()
 
 
 if __name__ == "__main__":
+    sys.path.append(os.path.join(os.getcwd(), "src"))
     load_dotenv()
     main()
